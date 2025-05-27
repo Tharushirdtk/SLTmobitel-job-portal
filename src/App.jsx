ab008deaac53da6e7df57f043580e810afd0e32d
Tharushirdtk <kavindirdtk@gmail.com>
2025-05-28 00:05:54 +0530

    create vacancies page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import Vacancies from './pages/Vacancies';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -22,6 +23,7 @@ function App() {
          <Route path="/about" element={<AboutUs/>} />
          <Route path="/login" element={<Login />} />
          <Route path="/register" element={<Registration />} />
+          <Route path="/vacancies" element={<Vacancies/>} />
       </Routes>
    
     </BrowserRouter>
---
+++ src/pages/Vacancies.jsx
@@ -0,0 +1,16 @@
+import React from "react"; 
+import VacansiesHero from "../components/VacanciesHero";
+import Footer from "../components/Footer"
+
+
+export default function Vacancies(){
+    return(
+        <>
+        
+       <VacansiesHero/>
+       <Footer/>
+      
+        </>
+        
+    )
+}
