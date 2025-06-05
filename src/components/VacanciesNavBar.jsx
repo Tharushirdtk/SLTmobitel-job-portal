176c5ecf7f851601155f55dacac9e19e444ed024
Tharushirdtk <kavindirdtk@gmail.com>
2025-06-05 21:43:15 +0530

    update vacancies navbar

--- src/components/VacanciesNavBar.jsx
+++ src/components/VacanciesNavBar.jsx
@@ -17,13 +17,12 @@ export default function VacanciesNavBar(){
 
       
    
-      <ul className="flex space-x-6 text-gray-700 font-medium">
+      <ul className="flex space-x-6 text-gray-700 font-bold">
         
         <li><Link to="/jobStaus">Job Status</Link></li>
         <li><Link to="/apply">Apply For Job</Link></li>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-
+      
       </ul>
     </nav>
         </>
