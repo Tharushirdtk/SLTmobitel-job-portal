c013569424ed406aa6edb200df60c2753ae2d915
Tharushirdtk <kavindirdtk@gmail.com>
2025-06-05 21:45:35 +0530

    update user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },  
+  email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 
@@ -18,5 +19,9 @@ userSchema.pre('save', async function(next) {
 userSchema.methods.comparePassword = function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
+
+
+
+
 
 module.exports = mongoose.model('User', userSchema);
